4a27a04409822979f4dc4fb1381cb42aa84824db
zev <zev@decent.xyz>
2022-08-17 20:03:11 -0400

    network config, mainnet, goerli, rinkeby

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,11 +4,25 @@ require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    mainnet: {
+      url: process.env.MAINNET_URL,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY as string]
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [process.env.PRIVATE_KEY as string]
+    },
+  },
   gasReporter: {
     currency: 'USD',
     coinmarketcap: process.env.COINMARKETCAP_KEY,
     enabled: false
-  }
+  },
 };
 
 export default config;

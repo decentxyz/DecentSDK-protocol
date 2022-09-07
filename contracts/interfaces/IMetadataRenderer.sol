f9fd2e18f330e3a0d0905e270b56e7201df784a5
Sweets Sweetman <sweetmantech@gmail.com>
2022-09-07 18:57:37 -0300

    I rollback removal of initializeWithData from interface.

--- contracts/interfaces/IMetadataRenderer.sol
+++ contracts/interfaces/IMetadataRenderer.sol
@@ -5,4 +5,13 @@ interface IMetadataRenderer {
     function tokenURI(uint256) external view returns (string memory);
 
     function contractURI() external view returns (string memory);
+
+    function initializeWithData(bytes memory initData) external;
+
+    /// @notice Storage for token edition information
+    struct TokenEditionInfo {
+        string description;
+        string imageURI;
+        string animationURI;
+    }
 }

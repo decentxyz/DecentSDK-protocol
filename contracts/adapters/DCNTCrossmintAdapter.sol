49319412127e03a500cfece91b58d5a97a343ec0
Sweets Sweetman <sweetmantech@gmail.com>
2023-02-22 15:52:20 -0300

    I update DCNTCrossmintAdapter to include target contract address to reduce CrossMint KYC requirements.

--- contracts/adapters/DCNTCrossmintAdapter.sol
+++ contracts/adapters/DCNTCrossmintAdapter.sol
@@ -12,26 +12,20 @@ pragma solidity ^0.8.0;
 (______/ (_______/(_______/(_______/|/    )_)   )_(
 
 */
-
 import {IDCNT721A} from "../interfaces/IDCNT721A.sol";
 
-contract CrossmintDecentAdapter  {
-    IDCNT721A public erc721;
-
-    constructor(address _erc721) {
-        erc721 = IDCNT721A(_erc721);
-    }
-
-    function mint(uint256 _quantity, address _to) public payable {
-         uint256 start = erc721.totalSupply();
+contract DCNTCrossmintAdapter  {
+    /// @notice mint target DCNT721A 
+    /// @param _target DCNT721A contract address
+    /// @param _quantity number of tokens
+    /// @param _to recipient of tokens
+    function mint(address _target, uint256 _quantity, address _to) public payable {
+        IDCNT721A erc721 = IDCNT721A(_target);
+        uint256 start = erc721.totalSupply();
         erc721.mint{value: msg.value}(_quantity);
         for (uint256 i = start; i < start + _quantity; i++) {
             erc721.transferFrom(address(this), _to, i);
         }
-    }
-
-    function totalSupply() public view returns(uint256 start) {
-        start = erc721.totalSupply();
     }
 
     /**
